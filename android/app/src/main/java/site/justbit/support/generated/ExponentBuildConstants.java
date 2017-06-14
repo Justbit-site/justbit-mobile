a9c33575e2409a9fe706b78e2da88a1012c148b1
SantiagoMolinaOrozco <santi020k@gmail.com>
2017-06-14 09:47:12 -0500

    Add Android package

---
+++ android/app/src/main/java/site/justbit/support/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package site.justbit.support.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp5ac6a3ae7acc4126aa37ac01d5ba5a7c://localhost:80";
+
+}
